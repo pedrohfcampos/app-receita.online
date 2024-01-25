b745cc63dc0790dd0c0c027c623b95988af0717d
pedrohfcampos <141269428+pedrohfcampos@users.noreply.github.com>
2024-01-25 20:53:27 -0300

    Delete Bariátrica natural directory

--- Bariátrica natural/first_page/js/recomendacaoPeso.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('formCalc').addEventListener('submit', function(event) {
-  const recomendacaoEbook = document.getElementById('inputPeso').value;
-
-  localStorage.setItem('recomendacaoEbook', recomendacaoEbook);
-});
